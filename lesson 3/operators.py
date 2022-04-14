e9ccdb9fe59f2655b871e4833f8539a98295925d
serhiinozdrin <serhiinozdrin@gmail.com>
2022-04-14 21:28:19 +0300

    Homework

---
+++ lesson 3/operators.py
@@ -0,0 +1,16 @@
+# бинарные операторы - два операнта - 2 + 3
+# унарные операторы + и -
+
+a = 0
+b = 42
+
+# result = None
+# if a != 0:
+#    result = a
+# else:
+#    result = b
+
+#print(result)
+
+result = a if a != 0 else b
+print(result)
