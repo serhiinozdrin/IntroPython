f0ca3bbdb7206757fe4658158a8e468a410cd03d
serhiinozdrin <serhiinozdrin@gmail.com>
2022-04-20 14:44:37 +0300

    homework

--- homework/find_cucumber.py
+++ homework/find_cucumber.py
@@ -1,18 +1,21 @@
 print('Щас найдем огурец 🥒')
 text = (input('Введите фразу со словом "cucumber": '))
-text.index('cucumber')
-text[text.index('cucumber')]
-print(text[text.index('cucumber'):])
+if 'cucumber' in text:
+    print(text[text.find('cucumber'):])
+else:
+    print('"cucumber" не найден... Ну блин, я ж просил 😢')
 
 
 
 
-# text.index('cucumber')
-# text[text.index('cucumber')]
+
+# print('Щас найдем огурец 🥒')
+# text = (input('Введите фразу со словом "cucumber": '))
+# if text.index('cucumber'):
+#     print(text[text.index('cucumber'):])
+# else:
+#     ('"cucumber" не найден... Ну блин, я ж просил 😢')
+
+# print('Щас найдем огурец 🥒')
+# text = (input('Введите фразу со словом "cucumber": '))
 # print(text[text.index('cucumber'):])
-
-
-
-
-# if text.index('cucumber') != 'cucumber':
-#     print('"cucumber" не найден... Ну блин, я ж просил 😢')
